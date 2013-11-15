78b530b0706f7a5a629e5ea85affe90ed592a7b9
bluejellybean <alexbarkell@gmail.com>
2013-11-15 10:15:16 -0500

    Added betting functions

--- CardGames/CardGames/Betting.cpp
+++ CardGames/CardGames/Betting.cpp
@@ -2,11 +2,36 @@
 #include <iostream>
 
 Betting::Betting(void){
+	betAmount = 10;
 	chipCount = 500;
 }
 
 void Betting::printChipCount(){
-	std::cout<<"Your chip count is: "<<chipCount<<std::endl;
+	std::cout<<"Chip count is: "<<chipCount<<std::endl;
+}
+void Betting::betChips(){
+	std::cin>>betAmount;
+	chipCount -= betAmount;
+}
+
+void Betting::winBet(){
+	chipCount = betAmount * 2;
+	betAmount = 10;
+}
+
+void Betting::LooseBet(){
+	//cout here?
+	betAmount = 10;
+}
+
+void Betting::winBlackJack(){
+	chipCount += betAmount * 2.5;
+	betAmount = 10;
+}
+
+void Betting::betPush(){
+	chipCount += betAmount;
+	betAmount = 10;
 }
 
 Betting::~Betting(void){
--- CardGames/CardGames/Betting.h
+++ CardGames/CardGames/Betting.h
@@ -1,8 +1,17 @@
 #pragma once
 class Betting
 {
+	int betAmount;
 public:
 	int chipCount;
+
+	void betChips();
+
+	void winBet();
+	void LooseBet();
+	void winBlackJack();
+	void betPush();
+
 
 	void printChipCount();
 

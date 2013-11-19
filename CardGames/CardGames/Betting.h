41a410b5e88ee782ea49b7f3cb10b58c364dfceb
bluejellybean <alexbarkell@gmail.com>
2013-11-19 14:01:20 -0500

    Added comments to Betting.h

--- CardGames/CardGames/Betting.h
+++ CardGames/CardGames/Betting.h
@@ -1,18 +1,25 @@
 #pragma once
 class Betting
 {
+	//used in paying out bets
 	int betAmount;
 	int chipCount;
 public:
+
+	//returns ChipCount
 	int getChipCount();
 
+	//bet amount gets set, chipCount -= betAmount
 	void betChips(int);
 
+	//TODO: setting betAmount to 10 everytime may be silly
+	//used in payouts
 	void winBet();
 	void LooseBet();
 	void winBlackJack();
 	void betPush();
 
+	//prints out the current chipCount
 	void printChipCount();
 
 	Betting(void);
